7b2a21a4f38a667b901b594c4e52d989623b4ac9
Nick Niemeir <nick.niemeir@gmail.com>
2013-04-29 06:38:22 +0000

    Simplify using convention

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var stream = require('stream')
   , Promise = require('promise')
   , streamCb = require('stream-cb')
+  , convention = require('convention')
 
 module.exports = convert
 convert.toStream = toStream
@@ -13,14 +14,11 @@ function convert (item) {
 
 function toStream (promise, noend) {
   if (typeof promise.pipe === 'function') return promise
-  var str = new stream.PassThrough
-  var cb = streamCb.toCb(str, noend)
 
-  promise.then(function (result) {
-    cb(null, result)
-  }, function (e) {
-    cb(e)
-  })
+  var str = new stream.PassThrough
+    , cb = convention.unwrap(streamCb.toCb(str, noend))
+
+  promise.then(cb.s, cb.s)
 
   return str
 }
@@ -29,9 +27,7 @@ function toPromise (st, encoding) {
   if (typeof st.then === 'function') return st
 
   return new Promise(function (resolve, reject) {
-    st.pipe(streamCb.toStream(function (e, result) {
-      if (e) return reject(e)
-      return resolve(result)
-    }, encoding))
+    st.pipe(streamCb.toStream(convention.wrap(reject, resolve), encoding))
   })
 }
+
